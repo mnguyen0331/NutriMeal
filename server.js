8591d30a8fa3253997275d5af9e4859b6dc1ea16
Euri Tanaka <dev@mugendiary.com>
2022-11-26 16:33:22 -0800

    added profile button

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ app.use(function(req, res, next) {
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
   res.locals.isAuthenticated = req.isAuthenticated();
+  res.locals.user = req.user
   next();
 });
 
